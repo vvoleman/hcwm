73ed5a62c9090e07f1f73131f05e959f63b3148b
vvoleman <vojtech.voleman@tul.cz>
2023-01-05 16:23:52 +0100

    fix conflicts

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-	header('Access-Control-Allow-Origin: http://localhost:8080');
+	header('Access-Control-Allow-Origin: *');
 //	header('Access-Control-Allow-Origin: *');
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
