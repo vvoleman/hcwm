c373e6571ba54edeb18ddb37e89cf479bc2c5310
vvoleman <vojtech.voleman@tul.cz>
2023-02-07 14:03:11 +0100

    Fix

--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
 	$corsUrl = $context['CORS_ALLOW_URL'] ?? 'localhost';
-
 	header('Access-Control-Allow-Origin: '. $corsUrl);
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
